7ffa421474a5a1fd5ef33db6e842fc3916347881
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-07 10:02:00 -0600

    Finalizando Movie Slider

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -7,8 +7,7 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 250,
-      color: Colors.red,
+      height: 260,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -18,6 +17,7 @@ class MovieSlider extends StatelessWidget {
             child: Text('Populares', style:  TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
           ),
 
+          SizedBox(height: 5),
           Expanded(
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
@@ -42,8 +42,36 @@ class _MoviePoster extends StatelessWidget {
     return Container(
        width: 130,
        height: 190,
-       color: Colors.green,
-      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+       
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          GestureDetector(
+            //NAvigator te lleva a otra pantalla. 
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
+            //ClipRRect genera bordes redondeados.
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'), 
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130,
+                height: 190,
+                fit:  BoxFit.cover,
+              ),
+            ),
+          ),
+          
+          SizedBox( height: 5),
+
+          Text(
+            'Strat Wars corre perroo canta baila por tu casa',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
     ) ;
     
   }
