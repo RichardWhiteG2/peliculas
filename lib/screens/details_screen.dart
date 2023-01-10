d81efd07bf710f1e52c7f32b91f17be6eb956871
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-09 20:21:18 -0600

    Slivers, SliverAppBarr, poster y titulo de pelicula

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -6,11 +6,99 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //Cambiar luego por una instacia de movie.
+
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body:  Container( 
-        child: Center(
-          child: Text("Details Screen"),
+      body:  CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle()
+              //Text('hola mundo')
+            ],
+            )
+          )
+        ],
+      ),
+      
+    );
+  }
+}class _CustomAppBar extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
         ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+
+  final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin:  EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+
+          SizedBox(width: 20),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('movie.title', style: textTheme.headline5, overflow: TextOverflow.ellipsis, maxLines: 2,),
+              Text('movie.originalTitle', style: textTheme.subtitle1, overflow: TextOverflow.ellipsis, maxLines: 1,),
+
+              Row(
+                children: [
+                  Icon(Icons.star_outline, size:15 , color: Colors.grey),
+                  SizedBox(width: 5),
+                  Text('movie.voteAverage', style: textTheme.caption,)
+                ],
+              )
+            ],
+          )
+        ]
       ),
     );
   }
