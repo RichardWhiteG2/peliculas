cd0aa3e09d4e2c8418b8be42d182cf42baa14b14
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-12 19:21:56 -0600

    Peticiones HTTP

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -2,9 +2,18 @@
 
 
 
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
+
+  String _apiKey   = 'b73afdb14d81ff27f916523d57fa5992';
+  String _baseUrl  = 'api.themoviedb.org';
+  String _language = 'es-ES';
 
   MoviesProvider(){
     print('MoviesProvider inicializado');
@@ -14,6 +23,16 @@ class MoviesProvider extends ChangeNotifier {
 
 //Peliculas a mostrar
   getOnDisplayMovies() async{
-    print('getOnDispayMovies');
+    var url =Uri.https(_baseUrl, '3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1'
+    });
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+
+    print(decodedData['dates']);
   }
 }
