9f9524452343fbe1cac5e3eeb76067ab923d6b36
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-17 19:28:19 -0600

    Preparando Lista de Actores

--- lib/widgets/casting_cards.dart
+++ lib/widgets/casting_cards.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 
 class CastingCards extends StatelessWidget {
   
+  final int movieId;
 
+  const CastingCards( this.movieId);
   @override
   Widget build(BuildContext context) {
     return Container(
