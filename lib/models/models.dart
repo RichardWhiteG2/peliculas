27dfcf8f401c3be76a60fa564b221b04a4bd7508
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-19 20:40:31 -0600

    Mostar peliculas basado en el query

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -10,8 +10,11 @@
 
 
 
+
+
 export 'package:peliculas/models/credits_response.dart';
 export 'package:peliculas/models/movie.dart';
 export 'package:peliculas/models/now_playing_response.dart';
 export 'package:peliculas/models/popular_response.dart';
+export 'package:peliculas/models/search_response.dart';
 export 'package:peliculas/models/upcoming_response.dart';
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -38,7 +38,7 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   Future<String> _getJsonData(String endpoint, [int page = 1] )async{
-     var url =Uri.https(_baseUrl, endpoint, {
+     final url =Uri.https(_baseUrl, endpoint, {
       'api_key': _apiKey,
       'language': _language,
       'page': '$page'
@@ -90,6 +90,18 @@ class MoviesProvider extends ChangeNotifier {
 
     return creditsResponse.cast;
 
+  }
+
+  Future<List<Movie>> searchMovies (String query) async{
+     final url =Uri.https(_baseUrl, '/3/search/movie', {
+      'api_key': _apiKey,
+      'language': _language,
+      'query': query
+    });
+    final response = await http.get(url);
+    final searchResponse = SearchResponse.fromJson(response.body);
+
+    return searchResponse.results;
   }
 
   getUpcomingMovies() async {
